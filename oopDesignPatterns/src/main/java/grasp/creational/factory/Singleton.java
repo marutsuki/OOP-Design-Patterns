704b9c1578bf8a5ae49bec6610b325935c215e7e
Lucien Lu <68990764+lucien7789@users.noreply.github.com>
2022-06-27 19:19:03 +1000

    Added singleton and prototype

---
+++ oopDesignPatterns/src/main/java/grasp/creational/factory/Singleton.java
@@ -0,0 +1,38 @@
+package grasp.creational.factory;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Pattern name: Singleton
+ *
+ * Problem: How do we enforce up to a single instance of a class? How do we allow global access to an object while following
+ * object-oriented paradigm?
+ *
+ * Solution: Create a class whose constructor can only be accessed by itself, which effectively restricts the user access on the
+ * object to what it defines itself.
+ *
+ * The class should hold a static instance of itself which it instantiates when required.
+ *
+ * Finally, a getInstance() method should be provided to the external user to allow them access to this one object.
+ */
+@Log4j2
+public class Singleton {
+
+    private static Singleton instance;
+
+    /**
+     * Note the private constructor is very important in ensuring that it is a "singleton"
+     */
+    private Singleton() {}
+
+    public void sayHello() {
+        log.info("Singleton says hello!");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
